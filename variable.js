87d2ea23e8ac02434e937eebe81bd77244c078d3
Harsh15551 <sahiljain15551@gmail.com>
2023-09-24 10:23:05 +0530

    adding variable related file in javascript

---
+++ variable.js
@@ -0,0 +1,9 @@
+
+let string='Harsh Kumar Kankariya';
+console.log(string)
+
+console.log(string.toLowerCase())
+
+let anotherString=new String('Sahil Jain');
+console.log(typeof anotherString);
+console.log(typeof string)
