893f85c4c67f02d510d9a5a26bb70e0869ba6727
Harsh Jain <hkankariya19@gmail.com>
2023-10-06 08:13:44 +0530

    deleting all files and starting nodejs second part chai with code

--- variable.js
+++
@@ -1,9 +0,0 @@
-
-let string='Harsh Kumar Kankariya';
-console.log(string)
-
-console.log(string.toLowerCase())
-
-let anotherString=new String('Sahil Jain');
-console.log(typeof anotherString);
-console.log(typeof string)
